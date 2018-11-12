0ae6dba205596669d07ecfa751c40fa4236253b2
SoheilSalehian <soheil.salehian@gmail.com>
2018-11-12 15:22:38 -0500

    Final

--- config.js
+++ config.js
@@ -16,18 +16,39 @@ exports.deactivate = function (oni) {
     console.log("config deactivated");
 };
 exports.configuration = {
-    //add custom config here, such as
-    "ui.colorscheme": "solarized8_dark",
+    activate: exports.activate,
+    "oni.hideMenu": "hidden",
+    "oni.loadInitVim": true,
     "oni.useDefaultConfig": false,
-    "editor.fontFamily": "Consolas",
-    "editor.fontSize": "13px",
-    "oni.loadInitVim": true,
+    "ui.colorscheme": "solarized8_dark",
+    "autoClosingPairs.enabled": true,
+    "commandline.mode": true,
+    "wildmenu.mode": false,
+    // "tabs.mode"                : "native", // Use vim's tabline, need completely quit Oni and restart a few times
+    "statusbar.enabled": false,
+    "sidebar.enabled": true,
+    "sidebar.default.open": false,
+    "learning.enabled": false,
+    "achievements.enabled": false,
+    "editor.typingPrediction": false,
     "editor.textMateHighlighting.enabled": false,
-    "environment.additionalPaths": ["/usr/local/go/bin"],
-    "oni.loadInitVim": true,
-    "editor.renderer": "webgl",
-    // UI customizations
     "ui.animations.enabled": true,
     "ui.fontSmoothing": "auto",
     "language.go.languageServer.command": ""
 };
+//export const configuration = {
+//    //add custom config here, such as
+//    "ui.colorscheme": "solarized8_dark",
+//    "oni.useDefaultConfig": false,
+//    "editor.fontFamily": "Consolas",
+//    "editor.fontSize": "13px",
+//     "oni.loadInitVim": true,
+//   "editor.textMateHighlighting.enabled": false,
+//   "environment.additionalPaths": ["/usr/local/go/bin"],
+//    "oni.loadInitVim": true,
+//    "editor.renderer": "webgl",
+//    // UI customizations
+//    "ui.animations.enabled": true,
+//    "ui.fontSmoothing": "auto",
+//    "language.go.languageServer.command": "",
+//}
--- config.tsx
+++ config.tsx
@@ -23,21 +23,43 @@ export const deactivate = (oni: Oni.Plugin.Api) => {
 }
 
 export const configuration = {
-    //add custom config here, such as
-
-    "ui.colorscheme": "solarized8_dark",
-    "oni.useDefaultConfig": false,
-    "editor.fontFamily": "Consolas",
-    "editor.fontSize": "13px",
-     "oni.loadInitVim": true,
-
-   "editor.textMateHighlighting.enabled": false,
-   "environment.additionalPaths": ["/usr/local/go/bin"],
-    "oni.loadInitVim": true,
-    "editor.renderer": "webgl",
-
-    // UI customizations
+    activate,
+    "oni.hideMenu"             : "hidden", // Hide top bar menu
+    "oni.loadInitVim"          : true, // Load user's init.vim
+    "oni.useDefaultConfig"     : false, // Do not load Oni's init.vim
+    "ui.colorscheme"           : "solarized8_dark", // Load init.vim colorscheme, remove this line if wants Oni's default
+    "autoClosingPairs.enabled" : true, // disable autoclosing pairs
+    "commandline.mode"         : true, // Do not override commandline UI
+    "wildmenu.mode"            : false, // Do not override wildmenu UI,
+    // "tabs.mode"                : "native", // Use vim's tabline, need completely quit Oni and restart a few times
+    "statusbar.enabled"        : false, // use vim's default statusline
+    "sidebar.enabled"          : true, // sidebar ui is gone
+    "sidebar.default.open"     : false, // the side bar collapse 
+    "learning.enabled"         : false, // Turn off learning pane
+    "achievements.enabled"     : false, // Turn off achievements tracking / UX
+    "editor.typingPrediction"  : false, // Wait for vim's confirmed typed characters, avoid edge cases
+    "editor.textMateHighlighting.enabled" : false, // Use vim syntax highlighting
     "ui.animations.enabled": true,
     "ui.fontSmoothing": "auto",
     "language.go.languageServer.command": "",
 }
+
+//export const configuration = {
+//    //add custom config here, such as
+
+//    "ui.colorscheme": "solarized8_dark",
+//    "oni.useDefaultConfig": false,
+//    "editor.fontFamily": "Consolas",
+//    "editor.fontSize": "13px",
+//     "oni.loadInitVim": true,
+
+//   "editor.textMateHighlighting.enabled": false,
+//   "environment.additionalPaths": ["/usr/local/go/bin"],
+//    "oni.loadInitVim": true,
+//    "editor.renderer": "webgl",
+
+//    // UI customizations
+//    "ui.animations.enabled": true,
+//    "ui.fontSmoothing": "auto",
+//    "language.go.languageServer.command": "",
+//}
